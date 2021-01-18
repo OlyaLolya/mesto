181d1093ef50fd99f3d4ec90aa0710dae0fe033d
OlyaLolya <oleka211999@mail.ru>
2021-01-18 17:59:57 +0300

    almost ready

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function toLike(icon){
+    icon.classList.toggle('card__icon-like_disabled');
+    icon.classList.toggle('card__icon-like_active');
+}
+function formOpen(){
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description')
+  let formHeading = document.querySelector('.form__input_heading');
+  let formDescription = document.querySelector('.form__input_description');
+
+  formHeading.value = profileName.innerHTML;
+  formDescription.value = profileDescription.innerHTML;
+
+  popUp.classList.add('popup__opened');
+}
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector('.form__input_heading');
+  let jobInput = document.querySelector('.form__input_description');
+
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  popUp.classList.remove('popup__opened');
+}
+
+let formElement = document.querySelector('.popup__form');
+
+let closeIcon = document.querySelector('.popup__icon-close');
+let popUp = document.querySelector('.popup');
+let editIcon = document.querySelector('.profile__edit-button');
+
+let likeIcon = document.querySelectorAll('.card__icon-like');
+
+closeIcon.addEventListener('click', () => {popUp.classList.remove('popup__opened')});
+editIcon.addEventListener('click', () => {formOpen()});
+
+
+
+for (let i=0; i < likeIcon.length; i++){
+  likeIcon[i].addEventListener('click', () => {toLike(likeIcon[i])});
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
