7ef50a70c422791ee5b7213d7445ea3d81130080
OlyaLolya <oleka211999@mail.ru>
2021-01-31 14:16:03 +0300

    fixed script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,33 @@
-function toLike(icon){
-    icon.classList.toggle('card__icon-like_disabled');
-    icon.classList.toggle('card__icon-like_active');
-}
-function formOpener(){
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description')
-  let formHeading = document.querySelector('.form__input_heading');
-  let formDescription = document.querySelector('.form__input_description');
+let nameInput = document.querySelector('.form__input_heading');
+let jobInput = document.querySelector('.form__input_description');
 
-  formHeading.value = profileName.innerHTML;
-  formDescription.value = profileDescription.innerHTML;
-
-  popUp.classList.add('popup__opened');
-}
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-
-  let nameInput = document.querySelector('.form__input_heading');
-  let jobInput = document.querySelector('.form__input_description');
-
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description');
-
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-  popUp.classList.remove('popup__opened');
-}
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
 let formElement = document.querySelector('.popup__form');
 
 let closeIcon = document.querySelector('.popup__icon-close');
-let popUp = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let editIcon = document.querySelector('.profile__edit-button');
 
-let likeIcon = document.querySelectorAll('.card__icon-like');
+function formOpener(){
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent
 
-closeIcon.addEventListener('click', () => {popUp.classList.remove('popup__opened')});
-editIcon.addEventListener('click', formOpener);
-
-for (let i = 0; i < likeIcon.length; i++){
-  likeIcon[i].addEventListener('click', () => {toLike(likeIcon[i])});
+  popup.classList.add('popup__opened');
 }
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closePopup();
+}
+function closePopup(){
+  popup.classList.remove('popup__opened');
+}
+
+closeIcon.addEventListener('click', () => closePopup());
+editIcon.addEventListener('click', formOpener);
 
 formElement.addEventListener('submit', formSubmitHandler);
